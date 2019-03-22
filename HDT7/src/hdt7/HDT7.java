858d1363729b2723340e85546ca5cdefd5410287
gera1013 <gera1013@users.noreply.github.com>
2019-03-21 22:03:32 -0600

    Version 1.5

--- HDT7/src/hdt7/HDT7.java
+++ HDT7/src/hdt7/HDT7.java
@@ -33,8 +33,8 @@ public class HDT7 {
         
         BinaryTree root = bst.addElements(bst, array);
         
-        //System.out.println("");
+        System.out.println("");
         
-        //bst.traverseInOrder(root);
+        bst.traverseInOrder(root);
     }
 }
