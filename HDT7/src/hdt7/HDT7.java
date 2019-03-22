089cadd80d96ca6eca0ffa81c39b73d31ef7a9c0
gera1013 <gera1013@users.noreply.github.com>
2019-03-21 22:03:22 -0600

    Version 1.5

--- HDT7/src/hdt7/HDT7.java
+++ HDT7/src/hdt7/HDT7.java
@@ -1,5 +1,10 @@
 package hdt7;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Gerardo
@@ -8,9 +13,28 @@ public class HDT7 {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws FileNotFoundException 
+    {
+        BinaryTree bst = new BinaryTree(null);
+        ArrayList<String> array = new ArrayList<>();
+        Scanner read = new Scanner(new File("doc.txt"));
+        read.useDelimiter("\\r\\n");
+        String data;
+        
+        while(read.hasNext())
+        {
+            if(read.hasNextLine()){
+                data = read.next();
+                if(!data.equals(" ")) array.add(data);
+            }
+        }
+        
+        BinaryTree root = bst.addElements(bst, array);
+        
+        //System.out.println("");
+        
+        //bst.traverseInOrder(root);
     }
-    
 }
